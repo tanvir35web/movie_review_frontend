4f36c58a3edaaa3f5613ebb8edf71577bc44ac66
tanvir35web <islammdtanvirul@gmail.com>
2025-01-11 23:20:13 +0600

    review api integration

--- app/components/HomePageComponents/TreandingMovies.jsx
+++ app/components/HomePageComponents/TreandingMovies.jsx
@@ -1,42 +1,47 @@
-'use client'
+"use client";
 import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
+import Link from "next/link";
 
 const TrendingMovies = () => {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        const fetchMovies = async () => {
-            try {
-                const response = await fetch("http://localhost:5000/api/movies/");
-                const data = await response.json();
-                setMovies(data);
-            } catch (error) {
-                console.error("Error fetching movies:", error);
-            }
-        };
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/movies/");
+        const data = await response.json();
+        setMovies(data);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    };
 
-        fetchMovies();
-    }, []);
+    fetchMovies();
+  }, []);
 
-    return (
-        <div>
-            <div>
-                <h1 className="text-2xl font-bold mt-10">Trending Movies</h1>
-                <div className="flex items-center space-x-6 mt-6 overflow-x-auto scrollbar-hide">
-                    {movies.map((movie) => (
-                        <MovieCard
-                            key={movie._id}
-                            posterImage={movie.poster_photo || "/stock-image/mov-4.jpg"}
-                            movieName={movie.title}
-                            rating={movie.rating}
-                            releaseYear={movie.release_date}
-                        />
-                    ))}
-                </div>
-            </div>
+  console.log("movies:", movies);
+
+  return (
+    <div>
+      <div>
+        <h1 className="text-2xl font-bold mt-10">Trending Movies</h1>
+        <div className="flex items-center space-x-6 mt-6 overflow-x-auto scrollbar-hide">
+          {movies.map((movie) => (
+            <Link href={`/${movie.movie_id}`} key={movie.movie_id}>
+              <MovieCard
+                key={movie.movie_id}
+                posterImage={movie.poster_photo || "/stock-image/mov-4.jpg"}
+                movieName={movie.title}
+                rating={movie.rating}
+                releaseYear={movie.release_date}
+              />
+            </Link>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default TrendingMovies;
